05775b837cd90bc5f7d4e902e95a0713064bc79c
Sasha Tatar <sasa.tatar@gmail.com>
2022-03-09 11:11:11 +0100

    Implement updateUser using context module function

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -76,6 +76,16 @@ function useUser() {
 // 🐨 add a function here called `updateUser`
 // Then go down to the `handleSubmit` from `UserSettings` and put that logic in
 // this function. It should accept: dispatch, user, and updates
+async function updateUser(dispatch, user, updates) {
+  dispatch({ type: 'start update', updates })
+  try {
+    const updatedUser = await userClient.updateUser(user, updates);
+    dispatch({ type: 'finish update', updatedUser });
+  } catch (error) {
+    dispatch({ type: 'fail update', error });
+    throw error;
+  }
+}
 
 // export {UserProvider, useUser}
 
@@ -98,11 +108,9 @@ function UserSettings() {
   function handleSubmit(event) {
     event.preventDefault()
     // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
+    updateUser(userDispatch, user, formState).catch(err => {
+      // ignore error
+    })
   }
 
   return (
