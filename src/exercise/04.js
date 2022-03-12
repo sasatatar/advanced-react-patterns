0c6ddfb272f5cc0141b55da912380de5811091b9
Sasha Tatar <sasa.tatar@gmail.com>
2022-03-12 07:22:58 +0100

    Implement getTogglerProps API

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -11,16 +11,32 @@ function useToggle() {
   // 🐨 Add a property called `togglerProps`. It should be an object that has
   // `aria-pressed` and `onClick` properties.
   // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  function getTogglerProps({ onClick, ...props } = {}) {
+    return {
+      'aria-pressed': on,
+      onClick() {
+        toggle();
+        onClick && onClick();
+      },
+      ...props
+    }
+  }
+  return {on, toggle, getTogglerProps}
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
